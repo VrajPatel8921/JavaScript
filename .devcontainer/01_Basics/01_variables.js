450168e15ab17bad2a8e11452f1101e8684c2a39
VrajPatel8921 <vrajp8921@gmail.com>
2025-01-28 14:36:05 +0000

    Varaibales

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accoutId=1234;
+let accountEmail="vraj@gmail.com";
+var accoutnPassward="12345";
+accountCity="Gandhinagar";
+let acoountState;
+
+// accoutId=5678;
+accountEmail="vp@vp.com";
+accoutnPassward="121212";
+accountCity="Jaipur";
+
+console.log(accoutId);
+
+console.table([accoutId,accountEmail,accoutnPassward,accountCity,acoountState])
