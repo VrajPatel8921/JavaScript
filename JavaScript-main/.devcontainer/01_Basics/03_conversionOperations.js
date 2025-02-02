a4e14c563e2baed01915a69c57bb17e3d8c09e39
Vraj <vrajp8921@gmail.com>
2025-02-02 22:07:51 +0530

    Operations

---
+++ JavaScript-main/.devcontainer/01_Basics/03_conversionOperations.js
@@ -0,0 +1,49 @@
+let Score = 33;
+console.log(typeof Score);
+let valueInNumber = Number(Score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN;
+//True=>1
+//False=>0
+//null=>0
+//undefinded=>NaN
+//String=>NaN
+
+
+let isLoggedIn = 1;
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(boolenIsLoggedIn);
+
+//1=>true
+//0=>false
+//""=>false
+//"Vraj"=>true
+
+//****** Operations **********//
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1="Hello";
+let str2=" Vraj";
+console.log(str1+str2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2" +2);
+console.log("01"+2 +2);
+console.log(1+2 +"2");
+
+console.log(true);
+console.log(+"");
+
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
