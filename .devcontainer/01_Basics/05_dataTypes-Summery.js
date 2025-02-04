a1f9c562307a0e824cd8c400e826c061d3cc8508
Vraj <vrajp8921@gmail.com>
2025-02-04 20:36:34 +0530

    dataTypes

---
+++ .devcontainer/01_Basics/05_dataTypes-Summery.js
@@ -0,0 +1,31 @@
+//Primitive
+//7 Types:String,Number,Boolean,null,undefined,symbol,bigInt;
+
+const Score=100;
+const ScoreValue=100.3;
+
+const isLoggedIn=true;
+let userEmail;
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+
+console.log(id==anotherId);
+
+
+//Reference (Non-Primitive)
+//Array,Object,Function
+
+let arr=["Vraj","IronMan","Thor"];
+let obj={
+    name:"Vraj",
+    age:19
+}
+
+console.log(arr);
+console.log(obj);
+
+// let myFun=Function(){
+//     console.log("Hello");
+// }
+
