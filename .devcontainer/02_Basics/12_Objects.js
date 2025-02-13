9a1a78cbe01142d3ff2ccc6a824d46b2aba7f9fb
Vraj <vrajp8921@gmail.com>
2025-02-13 20:43:48 +0530

    Object 2

---
+++ .devcontainer/02_Basics/12_Objects.js
@@ -0,0 +1,63 @@
+let tenderUser={};
+
+tenderUser.id="123abc";
+tenderUser.name="Vraj";
+tenderUser.isLoggedIn=false;
+
+console.log(tenderUser);
+
+let regulerUser={
+    fullName:{
+        userFullName:{
+            firstName:"Vraj",
+            lastName:"Patel"
+        }
+    }
+}
+
+console.log(regulerUser.fullName.userFullName.firstName);
+
+let obj1={1:"a",2:"b"};
+let obj2={3:"c",4:"d"};
+
+// let obj3=Object.assign({},obj1,obj2);
+// console.log(obj3);
+
+let obj3={...obj1,...obj2};
+console.log(obj3);
+
+let users=[
+    {
+        id:1,
+        email:"123xyz"
+    },
+    {
+        id:1,
+        email:"123xyz"
+    },
+    {
+        id:1,
+        email:"123xyz"
+    },
+    {
+        id:1,
+        email:"123xyz"
+    },
+    {
+        id:1,
+        email:"123xyz"
+    },
+    {
+        id:1,
+        email:"123xyz"
+    }
+]
+
+console.log(users[0].email);
+
+console.log(tenderUser);
+
+console.log(Object.keys(tenderUser));
+console.log(Object.values(tenderUser));
+console.log(Object.entries(tenderUser));
+console.log(Object.hasOwnProperty('name'));
