5d4fb701e03957b2cd263670583a73e366393c4a
Vraj <vrajp8921@gmail.com>
2025-03-15 20:39:12 +0530

    set Interval

---
+++ .devcontainer/08_Events/three.js
@@ -0,0 +1,23 @@
+const changeNameStart=()=>{
+    document.querySelector('h1').innerHTML="Set Interval Start";
+    console.log("Interval Start");
+    
+}
+const changeNameStop=()=>{
+    document.querySelector('h1').innerHTML="Set Interval Stop";
+    console.log("Interval Stop");
+    
+}
+
+let intervalInfo=setInterval(changeNameStart,2000);
+
+document.querySelector("#start").addEventListener('click',()=>{
+    changeNameStart();
+    setInterval(intervalInfo);
+})
+
+document.querySelector("#stop").addEventListener('click',()=>{
+    changeNameStop();
+    clearInterval(intervalInfo);
+})
+
