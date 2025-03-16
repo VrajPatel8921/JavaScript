9477b4883169f18c35b1b07fef5a535cdecae309
Vraj <vrajp8921@gmail.com>
2025-03-16 21:14:10 +0530

    Color Generator

---
+++ .devcontainer/07_DOM_4_Projects/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,13 @@
+// Generate Random color
+
+const randomColor=()=>{
+    let hex='0123456789ABCDEF';
+    let color='#';
+    for(let i=0;i<6;i++){
+        color +=hex[Math.floor(Math.random()*16)];      
+    }
+    return color;
+}
+
+document.querySelector('#start').addEventListener('click',stratChanginColor);
+
