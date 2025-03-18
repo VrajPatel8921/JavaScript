cd4f82086153a67cb4332299c96fb919c8bd5b92
Vraj <vrajp8921@gmail.com>
2025-03-18 20:00:12 +0530

    Asyn PRoject Completed

--- .devcontainer/07_DOM_4_Projects/6-unlimitedColors/chaiaurcode.js
+++ .devcontainer/07_DOM_4_Projects/6-unlimitedColors/chaiaurcode.js
@@ -8,8 +8,26 @@ const randomColor=()=>{
     }
     return color;
 }
+let intervalId;
+const stratChanginColor=()=>{
+    
+
+    const changeBgColor=()=>{
+        document.body.style.backgroundColor=randomColor();
+    }
+    
+    if(!intervalId){
+        intervalId=setInterval(changeBgColor,1000);
+    }
+}
+
+const stopChangingColor=()=>{
+    clearInterval(intervalId);
+    intervalId=null;
+}
 
 document.querySelector('#start').addEventListener('click',stratChanginColor);
+document.querySelector('#stop').addEventListener('click',stopChangingColor);
 
 
 
